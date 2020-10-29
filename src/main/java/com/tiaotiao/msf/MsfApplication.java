209e25e7ce584cb928b7b4293896a65aa30df596
Hansiyuan131 <422295068@qq.com>
2020-10-29 18:15:32 +0800

    add jwt

--- src/main/java/com/tiaotiao/msf/MsfApplication.java
+++ src/main/java/com/tiaotiao/msf/MsfApplication.java
@@ -1,16 +1,17 @@
 package com.tiaotiao.msf;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author hansiyuan
  */
+@MapperScan(basePackages = {"com.tiaotiao.msf"})
 @SpringBootApplication
 public class MsfApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MsfApplication.class, args);
     }
-
 }
