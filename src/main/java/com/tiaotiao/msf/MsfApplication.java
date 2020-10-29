92e87775ac5696308e345cff1861bc40d0e31c0f
Hansiyuan131 <422295068@qq.com>
2020-10-29 15:29:16 +0800

    init

---
+++ src/main/java/com/tiaotiao/msf/MsfApplication.java
@@ -0,0 +1,16 @@
+package com.tiaotiao.msf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hansiyuan
+ */
+@SpringBootApplication
+public class MsfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tiaotiao/msf/MsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiaotiao.msf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
