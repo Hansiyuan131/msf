79157ceb3d8d49ce8c885072c7419b66ed872963
Hansiyuan131 <422295068@qq.com>
2021-01-07 18:15:44 +0800

    add Jasypt and jwt

--- src/test/java/com/tiaotiao/msf/MsfApplicationTests.java
+++ src/test/java/com/tiaotiao/msf/MsfApplicationTests.java
@@ -3,11 +3,14 @@ package com.tiaotiao.msf;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.sound.midi.Soundbank;
+
 @SpringBootTest
 class MsfApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("1111");
     }
 
 }
---
+++ src/test/java/com/tiaotiao/msf/controller/HelloControllerTest.java
@@ -0,0 +1,14 @@
+package com.tiaotiao.msf.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HelloControllerTest {
+
+    @Test
+    public void test1() {
+        System.out.println("1234566");
+    }
+
+}
